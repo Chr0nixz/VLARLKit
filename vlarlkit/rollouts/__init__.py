f344c750ef9411f04f90126ad096173e1c87824e
yihaosun1124 <yihao.sun@mila.quebec>
2026-06-08 20:46:59 -0400

    add __init__

--- vlarlkit/rollouts/__init__.py
+++ vlarlkit/rollouts/__init__.py
@@ -1,4 +1,11 @@
 from vlarlkit.rollouts.rollout import Rollout
-from vlarlkit.rollouts.branch_rollout import BranchRollout
 
 __all__ = ["Rollout", "BranchRollout"]
+
+
+def __getattr__(name: str):
+    if name == "BranchRollout":
+        from vlarlkit.rollouts.branch_rollout import BranchRollout
+
+        return BranchRollout
+    raise AttributeError(name)
